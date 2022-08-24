cb4a83aa50bc335aba88c641c2be028e8477242c
Fagner <frodriguesbetim@gmail.com>
2022-08-24 19:06:09 -0300

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const themeDark = 'theme-dark'
+const themeLight = 'theme-light'
+
+function getTheme() {
+  const theme = localStorage.getItem('theme')
+  if (theme === 'null') return themeDark
+
+  return theme
+}
+function setTheme(theme) {
+  localStorage.setItem('theme', theme)
+  document.documentElement.className = theme
+}
+function setSlider(theme) {
+  const checked = theme === themeDark ? false : true
+  document.getElementById('slider').checked = true
+}
+function toggleTheme() {
+  const theme = getTheme()
+
+  if (theme === themeDark) {
+    setTheme(themeLight)
+    return
+  }
+
+  setTheme(themeDark)
+}
+window.onload = function () {
+  const theme = getTheme()
+  setTheme(theme)
+  setSlider(theme)
+}
